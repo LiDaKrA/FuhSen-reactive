93db053c95fa2b43203154b212cf14d7602ef3bf
Camilo Morales <Camilo.Jose.Morales.Rodriguez@iais.fraunhofer.de>
2016-04-01 19:43:10 +0200

    Transform to CSV (;) format.

--- app/utils/dataintegration/RDFUtil.scala
+++ app/utils/dataintegration/RDFUtil.scala
@@ -15,15 +15,12 @@
  */
 package utils.dataintegration
 
-import java.io.{ByteArrayInputStream, StringWriter}
-
+import java.io._
 import org.apache.jena.query.Dataset
-import org.apache.jena.rdf.model.{ModelFactory, Model}
+import org.apache.jena.rdf.model.{Model, ModelFactory, Statement, StmtIterator}
 import org.apache.jena.graph.Triple
-import org.apache.jena.riot.{RDFLanguages, Lang, RDFDataMgr}
-
-import scala.collection.mutable.ArrayBuffer
-import play.Logger
+import org.apache.jena.riot.{Lang, RDFDataMgr, RDFLanguages}
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 /**
   * Created on 2/29/16.
@@ -38,9 +35,13 @@ object RDFUtil {
 
   def modelToTripleString(model: Model,
                           lang: Lang): String = {
-    val output = new StringWriter()
-    RDFDataMgr.write(output, model, lang)
-    output.toString()
+    lang.toString match {
+      case csv if csv.toString == "Lang:CSV" => modelToCSV(model)
+      case _ =>
+        val output = new StringWriter()
+        RDFDataMgr.write(output, model, lang)
+        output.toString()
+    }
   }
 
   def stringToTriple(rdfContent: String, lang: Lang): Traversable[Triple] = {
@@ -79,4 +80,38 @@ object RDFUtil {
   implicit def stringToLang(langName: String): Lang = {
     langNameToLang(langName)
   }
+
+  def modelToCSV(model: Model): String = {
+    val statements : StmtIterator = model.listStatements()
+    var csv_pre_rows = new ListBuffer[Person_Helper]()
+    while(statements.hasNext) {
+      val current_stm : Statement = statements.nextStatement
+      csv_pre_rows.find(curr_person => current_stm.getSubject.toString.contains(curr_person.id)) match {
+        case matched_person : Some[Person_Helper] =>
+          populatePerson(current_stm.getPredicate.toString, matched_person.get, current_stm)
+        case None => //It is a new person.
+          val temp_helper = current_stm.getSubject.toString.split("/")
+          var new_person_helper = new Person_Helper(temp_helper(temp_helper.size - 1), "", "", "", "")
+          populatePerson(current_stm.getPredicate.toString, new_person_helper, current_stm)
+          csv_pre_rows += new_person_helper
+      }
+    }
+    var result_str = ""
+    csv_pre_rows.map( current =>
+      result_str += current.id+";"+current.name+";"+current.family_name+";"+current.given_name+";"+current.img+sys.props("line.separator")
+    )
+    result_str
+  }
+
+  def populatePerson(property:String, person: Person_Helper, current_stm : Statement) : Unit ={
+      property match {
+        case a if a == "http://xmlns.com/foaf/0.1/name" => person.name = current_stm.getObject.toString
+        case b if b == "http://xmlns.com/foaf/0.1/family_name" => person.family_name = current_stm.getObject.toString
+        case c if c == "http://xmlns.com/foaf/0.1/givenname" => person.given_name = current_stm.getObject.toString
+        case d if d == "http://xmlns.com/foaf/0.1/img" => person.img = current_stm.getObject.toString
+        case __ =>
+        }
+  }
 }
+
+case class Person_Helper(var id: String, var name: String, var family_name: String, var given_name: String, var img: String)
