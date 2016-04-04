fb7b79ede13aba6c54a79a50c27135633ed5dc7d
Luigi Selmi <luigiselmi@gmail.com>
2016-04-04 11:37:51 +0200

    minor changes

--- start_fuhsen.sh
+++ start_fuhsen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Script to run Fuhsen in a docker container.
 
-# Run Silk
+# Run Silk in background
 cd /home/silk-workbench-2.7.1/bin/
 ./silk-workbench -Dhttp.port=9005 &
 
-# Run Fuhsen
+# Run Fuhsen in background
 cd /home/lidakra/fuhsen-1.0-SNAPSHOT/bin/
 ./fuhsen & 
