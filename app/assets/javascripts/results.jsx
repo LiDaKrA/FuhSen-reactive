eecbfe084ac2daa70939b6d82c067e0821969cb1
Camilo Morales <Camilo.Jose.Morales.Rodriguez@iais.fraunhofer.de>
2016-04-28 17:18:58 +0200

    Adding functionality to the React components.

--- app/assets/javascripts/results.jsx
+++ app/assets/javascripts/results.jsx
@@ -1,45 +1,73 @@
 var Container = React.createClass({
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function () {
         return (
-            <LogicKeeper></LogicKeeper>
+            <div className="row search-results-container">
+                <Facets></Facets>
+                <ResultsContainer data={this_data}></ResultsContainer>
+            </div>
         );
     }
 });
 
-var LogicKeeper = React.createClass({
+var Facets = React.createClass({
     render: function () {
         return (
-            <div className="row search-results-container">
-                <div className="col-md-3 facets-container hidden-phone">
-                    <div className="facets-head">
-                        <h3>Ergebnisse filtern</h3>
+            <div className="col-md-3 facets-container hidden-phone">
+                <div className="facets-head">
+                    <h3>Ergebnisse filtern</h3>
+                </div>
+                <div className="js facets-list bt bb">
+                    <div className="facets-item bt bb bl br">
+                        <a className="h3" href="#" data-fctname="person_gender_fct">Geschlecht</a>
                     </div>
-                    <div className="js facets-list bt bb">
-                        <div className="facets-item bt bb bl br">
-                            <a className="h3" href="#" data-fctname="person_gender_fct">Geschlecht</a>
-                        </div>
-                        <div className="facets-item bt bb bl br">
-                            <a className="h3" href="#" data-fctname="person_birthday_fct">Geburtstag</a>
-                        </div>
+                    <div className="facets-item bt bb bl br">
+                        <a className="h3" href="#" data-fctname="person_birthday_fct">Geburtstag</a>
+                    </div>
 
-                        <div className="facets-item bt bb bl br">
-                            <a className="h3" href="#" data-fctname="person_occupation_fct">Beruf</a>
-                        </div>
+                    <div className="facets-item bt bb bl br">
+                        <a className="h3" href="#" data-fctname="person_occupation_fct">Beruf</a>
+                    </div>
 
-                        <div className="facets-item bt bb bl br">
-                            <a className="h3" href="#" data-fctname="person_livesat_fct">Lebt in</a>
-                        </div>
+                    <div className="facets-item bt bb bl br">
+                        <a className="h3" href="#" data-fctname="person_livesat_fct">Lebt in</a>
+                    </div>
 
-                        <div className="facets-item bt bb bl br">
-                            <a className="h3" href="#" data-fctname="person_worksat_fct">Arbeitet bei</a>
-                        </div>
+                    <div className="facets-item bt bb bl br">
+                        <a className="h3" href="#" data-fctname="person_worksat_fct">Arbeitet bei</a>
+                    </div>
 
-                        <div className="facets-item bt bb bl br">
-                            <a className="h3" href="#" data-fctname="person_studiesat_fct">Studium an</a>
-                        </div>
+                    <div className="facets-item bt bb bl br">
+                        <a className="h3" href="#" data-fctname="person_studiesat_fct">Studium an</a>
                     </div>
                 </div>
+            </div>
+        );
+    }
+});
 
+var ResultsContainer = React.createClass({
+    render: function () {
+        return (
                 <div className="col-md-9">
                     <div id="results-paginator-options" className="results-paginator-options">
                         <div class="off result-pages-count"></div>
@@ -69,32 +97,35 @@ var LogicKeeper = React.createClass({
                             </div>
                         </div>
                     </div>
-
                     <div className="search-results-content">
                         <div className="row">
                             <div className="col-md-12">
                                 <ul id="search-results" className="search-results">
                                     <ul className="results-list list-unstyled">
-                                        <ResultElement
-                                            name="Diego Collarana"
-                                            gender="Male"
-                                            addrss="Bonn/DE"
-                                            url="https://pbs.twimg.com/profile_images/2175489201/Dic01.png"
-                                            social_src="http://localhost:9000/assets/images/datasources/googleplus.png">
-                                        </ResultElement>
-                                        <ResultElement
-                                            name="Camilo Morales"
-                                            gender="Male"
-                                            addrss="Bonn/DE"
-                                            url="https://scontent.xx.fbcdn.net/hprofile-xpa1/v/t1.0-1/p50x50/21171_10155422530195055_1584223177974390414_n.jpg?oh=87385fa8dc9a98da04bb2b135292a74f&oe=57C036F3"
-                                            social_src="http://localhost:9000/assets/images/datasources/facebook.png">
-                                        </ResultElement>
+                                        <ResultsList data={this.props.data}>
+                                        </ResultsList>
                                     </ul>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
+        );
+    }
+});
+
+var ResultsList = React.createClass({
+    render: function() {
+        var resultsNodes = this.props.data["@graph"].map(function (result) {
+            return (
+                <ResultElement img={result.img} url={result.url}>
+                </ResultElement>
+            );
+        });
+
+        return (
+            <div className="commentList">
+                {resultsNodes}
             </div>
         );
     }
@@ -107,15 +138,23 @@ var ResultElement = React.createClass({
                 <div className="summary row">
                     <div className="thumbnail-wrapper col-md-2">
                         <div className="thumbnail">
-                            <img src={this.props.url} height="60px" width="75px"/>
+                            <img src={this.props.img} height="60px" width="75px"/>
                         </div>
                     </div>
                     <div className="summary-main-wrapper col-md-8">
-                        <TableRow name={this.props.name} gender={this.props.gender} addrss={this.props.addrss}></TableRow>
+                        <div className="summary-main">
+                            <h2 className="title">
+                                Some ID
+                            </h2>
+                            <div className="subtitle">
+                                <p>Gender: Some Gender.</p>
+                                <p>Address: Some Address.</p>
+                            </div>
+                        </div>
                     </div>
                     <div class="thumbnail-wrapper col-md-1">
                         <div class="thumbnail">
-                            <img src={this.props.social_src} alt="Information from FB" height="45" width="45"/>
+                            <img src={this.props.url} alt="Information from FB" height="45" width="45"/>
                         </div>
                     </div>
                 </div>
@@ -124,22 +163,52 @@ var ResultElement = React.createClass({
     }
 });
 
-var TableRow = React.createClass({
-    render: function () {
-        return (
-            <div className="summary-main">
-                <h2 className="title">
-                    {this.props.name}
-                </h2>
-                <div className="subtitle">
-                    <p>Gender: {this.props.gender}</p>
-                    <p>Address: {this.props.addrss}</p>
-                </div>
-            </div>
-        );
+var this_data={ "@graph" : [
+    {	"http://vocab.cs.uni-bonn.de/fuhsen#alias":"Diego Collarana",
+        "gender":"Male",
+        "addrss":[ "http://vocab.cs.uni-bonn.de/fuhsen#SearchableEntity", "http://xmlns.com/foaf/0.1/Person" ],
+        "img":"https://pbs.twimg.com/profile_images/2175489201/Dic01.png",
+        "url":"http://localhost:9000/assets/images/datasources/googleplus.png"
+    },
+    {
+        "http://vocab.cs.uni-bonn.de/fuhsen#alias":"Camilo Morales",
+        "gender":"Male",
+        "addrss": [ "http://vocab.cs.uni-bonn.de/fuhsen#SearchableEntity", "http://xmlns.com/foaf/0.1/Person" ],
+        "img":"https://scontent.xx.fbcdn.net/hprofile-xpa1/v/t1.0-1/p50x50/21171_10155422530195055_1584223177974390414_n.jpg?oh=87385fa8dc9a98da04bb2b135292a74f&oe=57C036F3",
+        "url":"http://localhost:9000/assets/images/datasources/facebook.png"
     }
-});
+],
+    "@context" : {
+        "alias" : {
+            "@id" : "http://vocab.cs.uni-bonn.de/fuhsen#alias",
+            "@type" : "http://www.w3.org/2001/XMLSchema#string"
+        },
+        "id" : {
+            "@id" : "http://vocab.cs.uni-bonn.de/fuhsen#id",
+            "@type" : "http://www.w3.org/2001/XMLSchema#double"
+        },
+        "lang" : {
+            "@id" : "http://vocab.cs.uni-bonn.de/fuhsen#lang",
+            "@type" : "http://www.w3.org/2001/XMLSchema#string"
+        },
+        "location" : {
+            "@id" : "http://vocab.cs.uni-bonn.de/fuhsen#location",
+            "@type" : "http://www.w3.org/2001/XMLSchema#string"
+        },
+        "url" : {
+            "@id" : "http://vocab.cs.uni-bonn.de/fuhsen#url",
+            "@type" : "@id"
+        },
+        "img" : {
+            "@id" : "http://xmlns.com/foaf/0.1/img",
+            "@type" : "@id"
+        },
+        "name" : {
+            "@id" : "http://xmlns.com/foaf/0.1/name",
+            "@type" : "http://www.w3.org/2001/XMLSchema#string"
+        }
+    }
+};
 
-React.render(<Container />, document.getElementById('skeleton'));
-
+React.render(<Container url="/ldw/v1/restApiWrapper/id/twitter/search?query=Camilo" pollInterval={200000}/>, document.getElementById('skeleton'));
 React.render(<SearchForm id_class="form-search-header"/>, document.getElementById('searchform'));
