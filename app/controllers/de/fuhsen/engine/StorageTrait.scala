8e8123791e693d35fee4db5614238a5bab252718
collarad <collarad@gmail.com>
2016-06-06 03:04:10 +0200

    Search engine service improved by type (person implemented)

---
+++ app/controllers/de/fuhsen/engine/StorageTrait.scala
@@ -0,0 +1,47 @@
+package controllers.de.fuhsen.engine
+
+/**
+  * Created by dcollarana on 6/5/2016.
+  */
+
+import org.apache.jena.rdf.model.Model
+import scala.collection.mutable.HashMap
+import play.Logger
+
+trait StorageTrait {
+
+  def saveModel(uid :String, model : Model)
+
+  def deleteModel(uid :String)
+
+  def getModel(uid :String) : Option[Model]
+
+  def size() : Integer
+
+}
+
+object GraphResultsCache extends StorageTrait {
+
+  private var searchList = new HashMap[String, Model]
+
+  override def saveModel(uid: String, model: Model) {
+    searchList.+=((uid, model))
+  }
+
+  override def deleteModel(uid: String): Unit = {
+    searchList.get(uid) match {
+      case Some(model) =>
+        model.close()
+        searchList.-=(uid)
+      case None =>
+        Logger.warn("No model found, nothing was deleted")
+    }
+  }
+
+  override def getModel(uid: String): Option[Model] = searchList.get(uid)
+
+  override def size(): Integer = {
+    searchList.size
+  }
+
+}
