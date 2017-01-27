34c6b3763e37162fcce6be7067cf6ca47caef47f
“CamiloMorales” <camilo.morales@gmail.com>
2017-01-27 04:40:46 +0100

    Add facets export to CSV file.

--- app/assets/javascripts/results.jsx
+++ app/assets/javascripts/results.jsx
@@ -271,6 +271,108 @@ var FacetList = React.createClass({
             this.loadFacetsFromServer(this.props.entityType,nextProps.orgFacetsDict);
         }
     },
+    facets2CSV: function () {
+        var JSONData = JSON.stringify(this.state.data);
+        var ReportTitle = "Current results in CSV format"
+        var ShowLabel = true;
+
+        //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+        var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+        var CSV = '';
+        //Set Report title in first row or line
+
+        //CSV += ReportTitle + '\r\n\n';
+
+        //This condition will generate the Label/Header
+        if (ShowLabel) {
+            var row = "";
+
+            //This loop will extract the label from 1st index of on array
+            for (var index in arrData[0]) {
+
+                //Now convert each value to string and comma-seprated
+                row += index + ',';
+            }
+
+            row = row.slice(0, -1);
+
+            //append Label row with line break
+            CSV += row + '\r\n';
+        }
+
+        //1st loop is to extract each row
+        for (var i = 0; i < arrData.length; i++) {
+            if (this.state.selectedChecks === undefined || this.state.selectedChecks === null || this.state.selectedChecks.length == 0 || this.state.selectedChecks.indexOf(i) > -1) {
+                var row = "";
+                var value_number_pair;
+                //2nd loop will extract each column and convert it in string comma-seprated
+                for (var index in arrData[i]) {
+                    if(index==="http://vocab.lidakra.de/fuhsen/hasFacet"){
+                        if(Object.prototype.toString.call(arrData[i][index]) === '[object Array]'){
+                            for (var j = 0; j < arrData[i][index].length; j++){
+                                value_number_pair=arrData[i][index][j].split("^")
+                                for (var k = 0; k < value_number_pair.length; k++){
+                                    row += '"' + value_number_pair[k] + '",';
+                                }
+                            }
+                        }else{
+                            value_number_pair=arrData[i][index].split("^")
+                            for (var k = 0; k < value_number_pair.length; k++){
+                                row += '"' + value_number_pair[k] + '",';
+                            }
+                        }
+                    }else{
+                        row += '"' + arrData[i][index] + '",';
+                    }
+                }
+
+                row.slice(0, row.length - 1);
+
+                //add a line break after each row
+                CSV += row + '\r\n';
+            }
+        }
+
+        if (CSV == '') {
+            alert("Invalid data");
+            return;
+        }
+
+        //Generate a file name
+        var fileName = "Fuhsen_Facets_";
+        //this will remove the blank-spaces from the title and replace it with an underscore
+        fileName += ReportTitle.replace(/ /g, "_");
+
+        //Initialize file format you want csv or xls
+        var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+        // Now the little tricky part.
+        // you can use either>> window.open(uri);
+        // but this will not work in some browsers
+        // or you will not get the correct file extension
+
+        //this trick will generate a temp <a /> tag
+        var link = document.createElement("a");
+        link.href = uri;
+
+        //set the visibility hidden so it will not effect on your web-layout
+        link.style = "visibility:hidden";
+        link.download = fileName + ".csv";
+
+        //this part will append the anchor tag and remove it after automatic click
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        this.setState({
+            resultsData: this.state.resultsData,
+            selected: this.state.selected,
+            loading: this.state.loading,
+            underDev: false,
+            view: this.state.view
+        });
+    },
     render: function () {
 
         if (this.state.data && this.state.data !== undefined) {
@@ -299,6 +401,9 @@ var FacetList = React.createClass({
                     <div className="js facets-list bt bb">
                         {MItems}
                     </div>
+                    <button onClick={this.facets2CSV}>
+                        Export Facets
+                    </button>
                 </div>
             )
         }
