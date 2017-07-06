157b0b33816695f414f5946eca73f771f6472d51
dcollarana <RadOffice01Git>
2017-07-06 15:12:06 -0400

    Minor fixes in ElasticSearch api cursoring

--- app/utils/loggers/DataLogger.scala
+++ app/utils/loggers/DataLogger.scala
@@ -1,8 +1,10 @@
 package utils.loggers
 
+import com.typesafe.config.ConfigFactory
 import org.apache.jena.query.{QueryExecutionFactory, QueryFactory}
 import org.apache.jena.rdf.model.Model
 import play.Logger
+
 import scala.collection.JavaConversions.asScalaIterator
 
 /**
@@ -13,7 +15,9 @@ object GraphLogger {
   val logger = Logger.of(GraphLogger.getClass)
 
   def log(graph: String) = {
-    logger.debug(graph)
+    if (ConfigFactory.load.getBoolean("fuhsen.logger.messages.enabled"))
+      logger.debug(graph)
+
   }
 }
 
@@ -22,34 +26,35 @@ object OrganizationLogger {
   val logger = Logger.of(OrganizationLogger.getClass)
 
   def log(model: Model) = {
-
-    val query = QueryFactory.create(
-      s"""
-         |PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-         |PREFIX fs: <http://vocab.lidakra.de/fuhsen#>
-         |PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-         |
+    if (ConfigFactory.load.getBoolean("fuhsen.logger.messages.enabled"))
+    {
+      val query = QueryFactory.create(
+        s"""
+           |PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+           |PREFIX fs: <http://vocab.lidakra.de/fuhsen#>
+           |PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+           |
          |SELECT ?organization
-         |WHERE {
-         |  ?s a foaf:Organization .
-         |  ?s fs:name ?organization
-         |}
-         |
+           |WHERE {
+           |  ?s a foaf:Organization .
+           |  ?s fs:name ?organization
+           |}
+           |
       """.stripMargin)
 
-    val resultSet = QueryExecutionFactory.create(query, model).execSelect()
+      val resultSet = QueryExecutionFactory.create(query, model).execSelect()
 
-    val organizations = resultSet.map { r =>
-      if (r.get("organization") != null && r.get("organization").isLiteral)
-        r.getLiteral("organization").getString
-      else
-        ""
-    }.filter( p => !p.isEmpty )
-      .toList
-      .mkString(", ")
+      val organizations = resultSet.map { r =>
+        if (r.get("organization") != null && r.get("organization").isLiteral)
+          r.getLiteral("organization").getString
+        else
+          ""
+      }.filter( p => !p.isEmpty )
+        .toList
+        .mkString(", ")
 
-    logger.debug(organizations)
-
+      logger.debug(organizations)
+    }
   }
 
 }
@@ -59,40 +64,40 @@ object LocationLogger {
   val logger = Logger.of(LocationLogger.getClass)
 
   def log(model: Model) = {
-
-    val query = QueryFactory.create(
-      s"""
-         |PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-         |PREFIX fs: <http://vocab.lidakra.de/fuhsen#>
-         |
+    if (ConfigFactory.load.getBoolean("fuhsen.logger.messages.enabled")) {
+      val query = QueryFactory.create(
+        s"""
+           |PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+           |PREFIX fs: <http://vocab.lidakra.de/fuhsen#>
+           |
          |SELECT ?location
-         |WHERE {
-         |  {
-         |     ?s a ?SearchableEntity .
-         |     ?s fs:location ?location
-         |  }
-         |  UNION
-         |  {
-         |     ?s a fs:Location .
-         |     ?s fs:name ?location .
-         |  }
-         |}
-         |
+           |WHERE {
+           |  {
+           |     ?s a ?SearchableEntity .
+           |     ?s fs:location ?location
+           |  }
+           |  UNION
+           |  {
+           |     ?s a fs:Location .
+           |     ?s fs:name ?location .
+           |  }
+           |}
+           |
       """.stripMargin)
 
-    val resultSet = QueryExecutionFactory.create(query, model).execSelect()
+      val resultSet = QueryExecutionFactory.create(query, model).execSelect()
 
-    val locations = resultSet.map { r =>
-      if (r.get("location") != null && r.get("location").isLiteral)
-        r.getLiteral("location").getString
-      else
-        ""
-    }.filter( p => !p.isEmpty )
-      .toList
-      .mkString(", ")
+      val locations = resultSet.map { r =>
+        if (r.get("location") != null && r.get("location").isLiteral)
+          r.getLiteral("location").getString
+        else
+          ""
+      }.filter( p => !p.isEmpty )
+        .toList
+        .mkString(", ")
 
-    logger.debug(locations)
-
+      logger.debug(locations)
+    }
   }
 
 }
