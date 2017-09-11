0ee5ce66cd157f4f5c07a12d03bf8b3ca859518c
Mayesha Tasnim <mayesha.tasnim@gmail.com>
2017-09-11 15:38:07 +0600

    Export and Clean button for Favorites

--- app/assets/javascripts/favorites.jsx
+++ app/assets/javascripts/favorites.jsx
@@ -36,6 +36,81 @@ var FavoritesContainer = React.createClass({
             }
         });
     },
+    csvFunction: function () {
+        var JSONData = JSON.stringify(this.state.data);
+        var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+        var ReportTitle = "Favorite results in CSV format"
+
+        var CSV = '';
+        var headers_set = new Set();
+
+        for (var i = 0; i < arrData.length; i++) {
+            if (this.state.selectedChecks === undefined || this.state.selectedChecks === null || this.state.selectedChecks.length == 0 || this.state.selectedChecks.indexOf(i) > -1) {
+                for (var header in arrData[i]) {
+                    headers_set.add(header)
+                }
+            }
+        }
+
+        let headers = Array.from(headers_set);
+        for (var i = 0; i < headers.length; i++) CSV += headers[i] + ',';
+        CSV = CSV.slice(0, -1);
+        CSV += '\r\n';
+
+        for (var i = 0; i < arrData.length; i++) {
+            if (this.state.selectedChecks === undefined || this.state.selectedChecks === null || this.state.selectedChecks.length == 0 || this.state.selectedChecks.indexOf(i) > -1) {
+                var row = "";
+
+                for (var index in headers) {
+                    //console.log(headers[index])
+                    var value = arrData[i][headers[index]]
+                    if( value === undefined || value === 'null') value=''
+                    row += '"' + value + '",';
+                }
+
+                row.slice(0, row.length - 1);
+
+                //add a line break after each row
+                CSV += row + '\r\n';
+            }
+        }
+
+        if (CSV == '') {
+            alert("Invalid data");
+            return;
+        }
+
+        //Generate a file name
+        var fileName = "Fuhsen_";
+        //this will remove the blank-spaces from the title and replace it with an underscore
+        fileName += ReportTitle.replace(/ /g, "_");
+
+        //Initialize file format you want csv or xls
+        var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+        // Now the little tricky part.
+        // you can use either>> window.open(uri);
+        // but this will not work in some browsers
+        // or you will not get the correct file extension
+
+        //this trick will generate a temp <a /> tag
+        var link = document.createElement("a");
+        link.href = uri;
+
+        //set the visibility hidden so it will not effect on your web-layout
+        link.style = "visibility:hidden";
+        link.download = fileName + ".csv";
+
+        //this part will append the anchor tag and remove it after automatic click
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        this.setState({
+            data: this.state.data,
+            searchUid: this.state.searchUid
+        });
+    },
     handleClean: function ()
     {
         var urlString = window.location.href.toString();
@@ -59,7 +134,7 @@ var FavoritesContainer = React.createClass({
     render: function () {
         var resultList = this.state.data;
         var results = {};
-        if(resultList !== undefined){
+        if(resultList !== undefined && resultList !== null){
             results = resultList.map(function (result,idx) {
                 return (
                     <FavouriteResult
@@ -85,13 +160,13 @@ var FavoritesContainer = React.createClass({
                     {results}
                 </div>
                 <div>
-                    <span className="btn btn-primary btn-file btn-md">
-                    {getTranslation("select_file")} <input type="file"></input>
-                    </span>
+                    <button className="btn btn-primary btn-md btn-fav" onClick={this.csvFunction}>
+                    {getTranslation("exporttocsv")}
+                    </button>
                     &nbsp;&nbsp;
-                    <span className="btn btn-primary btn-file btn-md">
-                    {getTranslation("clean_favorites")} <input type="file" onChange={this.handleClean}></input>
-                    </span>
+                    <button className="btn btn-primary btn-md btn-fav" onClick={this.handleClean}>
+                    {getTranslation("clean_favorites")}
+                    </button>
                 </div>
             </div>
 
